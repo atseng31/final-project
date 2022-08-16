b026c54b1782c9dde2053ad5865b13d27b348438
Alice <alicetseng31@icloud.com>
2022-08-16 14:38:43 -0400

    Initial Commit

---
+++ final project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final project
+//
+//  Created by Xing Lin on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
